33ac9d7d6ab9356db43b205a124d9fb8b9a8bbae
Rodrigue Koffi <bonclay7@users.noreply.github.com>
2024-05-14 22:21:59 +0200

    Introduce support for service account tokens

--- internal/pkg/aws/aws.go
+++ internal/pkg/aws/aws.go
@@ -14,13 +14,34 @@ import (
 type api interface {
 	ListWorkspaces(input *managedgrafana.ListWorkspacesInput) (*managedgrafana.ListWorkspacesOutput, error)
 	CreateWorkspaceApiKey(input *managedgrafana.CreateWorkspaceApiKeyInput) (*managedgrafana.CreateWorkspaceApiKeyOutput, error)
+	CreateWorkspaceServiceAccountToken(input *managedgrafana.CreateWorkspaceServiceAccountTokenInput) (*managedgrafana.CreateWorkspaceServiceAccountTokenOutput, error)
 	DeleteWorkspaceApiKey(input *managedgrafana.DeleteWorkspaceApiKeyInput) (*managedgrafana.DeleteWorkspaceApiKeyOutput, error)
-	//DescribeWorkspace(input *managedgrafana.DescribeWorkspaceInput) (*managedgrafana.DescribeWorkspaceOutput, error)
+	DeleteWorkspaceServiceAccountToken(input *managedgrafana.DeleteWorkspaceServiceAccountTokenInput) (*managedgrafana.DeleteWorkspaceServiceAccountTokenOutput, error)
+	DescribeWorkspace(input *managedgrafana.DescribeWorkspaceInput) (*managedgrafana.DescribeWorkspaceOutput, error)
 }
 
-// AMGApiKey holds the dataplane Grafana API key for a workspace
+type GrafanaAuth interface {
+	GetAuth() string
+}
+
+// AMGApiKey is a GrafanaAuth struct for Grafana API key
 type AMGApiKey struct {
 	KeyName, APIKey, WorkspaceID string
+}
+
+// GetAuth returns the API key for a workspace
+func (k AMGApiKey) GetAuth() string {
+	return k.APIKey
+}
+
+// AMGServiceAccountToken is a GrafanaAuth struct for service account tokens
+type AMGServiceAccountToken struct {
+	ServiceAccountID, SATokenID, Token, WorkspaceID string
+}
+
+// GetAuth returns the SA token for a workspace
+func (t AMGServiceAccountToken) GetAuth() string {
+	return t.Token
 }
 
 // AMG is a AWS SDK client for AMG apis
@@ -72,6 +93,23 @@ func (a *AMG) ListWorkspaces() ([]Workspace, error) {
 	return wx, nil
 }
 
+// DescribeWorkspace returns information about a workspace
+func (a *AMG) DescribeWorkspace(workspaceID string) (Workspace, error) {
+	res, err := a.Client.DescribeWorkspace(&managedgrafana.DescribeWorkspaceInput{
+		WorkspaceId: aws.String(workspaceID),
+	})
+	if err != nil {
+		return Workspace{}, err
+	}
+	w := Workspace{
+		ID:       workspaceID,
+		Name:     aws.StringValue(res.Workspace.Name),
+		Version:  aws.StringValue(res.Workspace.GrafanaVersion),
+		Endpoint: aws.StringValue(res.Workspace.Endpoint),
+	}
+	return w, nil
+}
+
 // CreateWorkspaceApiKey creates a new API key for a workspace
 //
 //revive:disable
@@ -104,6 +142,48 @@ func (a *AMG) DeleteWorkspaceApiKey(apiKey AMGApiKey) error {
 	_, err := a.Client.DeleteWorkspaceApiKey(&managedgrafana.DeleteWorkspaceApiKeyInput{
 		KeyName:     aws.String(apiKey.KeyName),
 		WorkspaceId: aws.String(apiKey.WorkspaceID),
+	})
+
+	if err != nil {
+		log.Error(err)
+	}
+	return err
+}
+
+func (a *AMG) CreateServiceAccountToken(workspaceID, serviceAccountID string) (AMGServiceAccountToken, error) {
+	log.Info()
+	log.InfoLight("Creating service account token for service account ", serviceAccountID)
+	currentTime := time.Now().UTC()
+	saTokenName := fmt.Sprintf("%s-%d", "amg-migrator", currentTime.UnixMilli())
+
+	duration := time.Duration(30 * 60 * time.Second)
+	resp, err := a.Client.CreateWorkspaceServiceAccountToken(&managedgrafana.CreateWorkspaceServiceAccountTokenInput{
+		Name:             aws.String(saTokenName),
+		SecondsToLive:    aws.Int64(int64(duration.Seconds())),
+		WorkspaceId:      aws.String(workspaceID),
+		ServiceAccountId: aws.String(serviceAccountID),
+	})
+
+	if err != nil {
+		return AMGServiceAccountToken{}, err
+	}
+
+	return AMGServiceAccountToken{
+		ServiceAccountID: serviceAccountID,
+		SATokenID:        *resp.ServiceAccountToken.Id,
+		Token:            *resp.ServiceAccountToken.Key,
+		WorkspaceID:      workspaceID,
+	}, nil
+}
+
+func (a *AMG) DeleteServiceAccountToken(saToken AMGServiceAccountToken) error {
+	log.Info()
+	log.InfoLight("Removing service account token for service account ", saToken.ServiceAccountID)
+
+	_, err := a.Client.DeleteWorkspaceServiceAccountToken(&managedgrafana.DeleteWorkspaceServiceAccountTokenInput{
+		ServiceAccountId: aws.String(saToken.ServiceAccountID),
+		TokenId:          aws.String(saToken.SATokenID),
+		WorkspaceId:      aws.String(saToken.WorkspaceID),
 	})
 
 	if err != nil {
